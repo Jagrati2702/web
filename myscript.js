c73f4ee380fcbf91e4df3841ad24b58f90de0f22
Jagrati Garg <101218823+Jagrati2702@users.noreply.github.com>
2023-03-03 14:11:25 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,16 @@
+//Javascript code//
+        
+        //Javascript statements//
+        //alert("This is JavaScript");
+
+        //variables//
+        var a=10;
+        a="Welcome to";
+        var b=20;
+        b=" JavaScript";
+        var c=a + b;
+        console.log(c);
+        document.write(c);
+        alert("The result is " +c);
+        document.getElementById("head1").innerText=c;
+        document.getElementById("head1").style.display="none";
